757d5d31bdb76c8434c4b65ade73a42b483ef07f
Masum Billah <sciencefiction844@gmail.com>
2026-08-22 23:13:13 +0600

    Implement onCreate to register AuroraMediaPlugin
    Added onCreate method to register AuroraMediaPlugin.

--- android/app/src/main/java/com/masumbillah/auroranexus/MainActivity.java
+++ android/app/src/main/java/com/masumbillah/auroranexus/MainActivity.java
@@ -1,5 +1,20 @@
 package com.masumbillah.auroranexus;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        registerPlugin(
+            AuroraMediaPlugin.class
+        );
+
+        super.onCreate(
+            savedInstanceState
+        );
+    }
+}
